67738677adbe3a7f8c7fe021f6c307b7b546c96b
Rumis <20.21.9.12.1bibi@gmail.com>
2023-11-06 10:15:58 +0700

    Fix column width get reset with the data also

--- src/main/java/GUI/NotificationPanel.java
+++ src/main/java/GUI/NotificationPanel.java
@@ -24,6 +24,9 @@ public class NotificationPanel extends ChildrenPanel {
         resetButton = getFormattedButton("Reset", 30, 60, 80, 24, new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 refreshTableScrollPane(notificationController.search(Integer.valueOf(f.account.getId())));
+                panelTable.getColumnModel().getColumn(0).setPreferredWidth(50);
+                panelTable.getColumnModel().getColumn(1).setPreferredWidth(580);
+                panelTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
             }
         });
         add(resetButton);
